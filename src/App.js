c6fba08caf78c2c674478ac0735014bec5a579f0
legloc <developer.aliev@gmail.com>
2020-12-20 22:02:35 +0300

    Created Checkbox component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Checkbox from './components/Checkbox'
 import './App.sass'
 
 const App = () => {
   return (
     <div className="wrapper">
       <h1 className="wrapper-title">Our pricing</h1>
+      <div className="payment-switcher">
+        <span className="payment-title">Annually</span>
+        <Checkbox />
+        <span className="payment-title">Monthly</span>
+      </div>
     </div>
   )
 }
---
+++ src/components/Checkbox.js
@@ -0,0 +1,10 @@
+const Checkbox = () => {
+  return (
+    <label className="checkboxWrapper">
+      <input className="checkboxInput" type="checkbox" />
+      <div className="checkboxSlider"></div>
+    </label>
+  )
+}
+
+export default Checkbox
